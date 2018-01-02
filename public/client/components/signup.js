a5ec7ce5b7f95fbade2f851201b080dd31b29339
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-01 22:23:28 -0600

    adds a redirect directly to the auth

--- public/client/components/signup.js
+++ public/client/components/signup.js
@@ -9,7 +9,7 @@ angular.module('main-app')
         $http.get('/api/auth/google')
           .then(function successCallback(response) {
             console.log(response, 'response from axios');
-            // $window.location.reload();
+            $window.redirect('https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3010%2Fapi%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=383698803465-o0645lph6b5dv86625e86c3a133imjf4.apps.googleusercontent.com');
 
           })
           .catch(function errorCallback(error) {
