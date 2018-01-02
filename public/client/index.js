110d1d7b6296ab6974ad51a684b0bf70ebcd907d
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-02 03:21:43 -0600

    tries to redirect the user using hashPrefix

--- public/client/index.js
+++ public/client/index.js
@@ -217,5 +217,6 @@ angular.module('main-app', ['ngRoute', 'ngResource', 'angularPayments'])
         .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('');
 
 })
