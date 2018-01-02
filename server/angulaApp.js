a9ea0ffc29ec2b4be620d32d674277bd7809f8c4
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-02 17:07:27 -0600

    adds hash prefix back into index.js

--- server/angulaApp.js
+++ server/angulaApp.js
@@ -294,9 +294,9 @@ app.get('/api/auth/google/callback',
   app.use('/', router);
 
 app.use('/*', express.static(path.join(__dirname, '../index.html')))
-  // app.use('/*', function(req, res) {
-  //  res.sendFile(__dirname.slice(0, __dirname.length - 6) + 'index.html');
-  // })
+  app.use('/*', function(req, res) {
+   res.sendFile(__dirname.slice(0, __dirname.length - 6) + 'index.html');
+  })
 
 
 module.exports = server;
