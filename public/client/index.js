e56928a3a529e2ca9187bca8d03bc3e8cb7d1331
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-02 18:24:00 -0600

    adds the data target

--- public/client/index.js
+++ public/client/index.js
@@ -218,6 +218,6 @@ angular.module('main-app', ['ngRoute', 'ngResource', 'angularPayments'])
         .otherwise({ redirectTo: '/' });
 
     // $locationProvider.html5Mode(true);
-    $locationProvider.hashPrefix('');
+    $locationProvider.hashPrefix('index.html');
 
 })
