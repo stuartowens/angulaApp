453405f01c6f03652feba598b180385ad3166aab
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-03 01:26:52 -0600

    changes the profile submit form

--- db/util.js
+++ db/util.js
@@ -59,7 +59,7 @@ function updateProfile(req, res) {
   console.log(req.body, 'req.body~~~~~~upon update$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
 
   // var id = req.id;
-  Profile.findOne({ _id: req._id, user_id: req.session.passport.user }, (err, profile) => {
+  Profile.findOne({ _id: req._id }, (err, profile) => {
     if (err) {
       console.log('~~~~~~~~~~~~~~~~~~~~~~~~error in updateProfile~~~~~~~~~~~~~~~~~~~~~~~~~~~````', err)
     } else {
