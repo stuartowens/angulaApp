71c7acb1bafed7e8cf69df5c6d6d569c9d2ba401
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-03 01:15:37 -0600

    changes the profile submit form

--- public/client/components/example.js
+++ public/client/components/example.js
@@ -12,22 +12,23 @@ angular.module('main-app')
         console.log($scope.vm, '$scope')
         console.log(this.profile, 'this.profile')
         console.log(this, 'this')
-        // $http({
-        //   method: 'PUT',
-        //   url: 'https://bandcamp.cc/api/updateUser/',
-        //   data: $scope.profile
-        // }).then(function successCallback(response) {
-        //     // this callback will be called asynchronously
-        //     // when the response is available
-        //     return response;
-        //   }, function errorCallback(response) {
-        //     // called asynchronously if an error occurs
-        //     // or server returns response with an error status.
-        //   }).then(function(res){
-        //     console.log(res, 'profileData')
-        //     $scope.profile = res
-        //     console.log($scope.profile, "during callback")
-        //   });
+        $http({
+          method: 'PUT',
+          url: 'https://bandcamp.cc/api/updateUser/',
+          data: this.profile
+        }).then(function successCallback(response) {
+            $('#editProfile').modal('hide');
+            // this callback will be called asynchronously
+            // when the response is available
+            return response;
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          }).then(function(res){
+            console.log(res, 'profileData')
+            $scope.profile = res
+            console.log($scope.profile, "during callback")
+          });
       }.bind(this)
     },
     controllerAs: 'vm',
