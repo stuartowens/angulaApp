823aa0a2c7b8b4840943687f6a9ec2f6f8ab62ec
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-05 01:33:57 -0600

    adds a stripe service provider

--- public/client/index.js
+++ public/client/index.js
@@ -8,7 +8,7 @@ var roles = {
 var unauthorizedAccessRoute = ' /UnauthorizedAccess';
 
 angular.module('main-app', ['ngRoute', 'ngResource', 'angularPayments'])
-.controller('MainCtrl', function($scope, $rootScope, $http, faqService, signinService, stripeService) {
+.controller('MainCtrl', function($scope, $rootScope, $http, faqService, signinService) {
   $rootScope.logout = function (){
     signinService.logout();
     delete $rootScope.user
@@ -98,7 +98,7 @@ angular.module('main-app', ['ngRoute', 'ngResource', 'angularPayments'])
           controllerAs: 'ctrl'
         })
         .when('/registration', {
-          controller: function($scope, $http, $rootScope, $location, signinService, stripeService) {
+          controller: function($scope, $http, $rootScope, $location, signinService) {
             this.user = $rootScope.user;
             $rootScope.logout = function (){
               signinService.logout();
@@ -110,7 +110,51 @@ angular.module('main-app', ['ngRoute', 'ngResource', 'angularPayments'])
               this.user = $rootScope.user;
               $rootScope.user.total = $rootScope.user.studentTotal * 379 + $rootScope.user.rvCampers * 279 + $rootScope.user.cabinCampers * 199 + $rootScope.user.tentCampers * 150 + $rootScope.user.chaperoneLunches*50 - $rootScope.user.amt_paid;
             }
-            $rootScope.handleCheckout = stripeService.handleCheckout
+            $rootScope.handleCheckout = function (address, token) {
+              $rootScope.user.address = address;
+              $rootScope.user.id = token;
+              $http({
+                method: 'PUT',
+                url: 'https://bandcamp.cc/api/updateUser/',
+                data: $rootScope.user
+              }).then(function successCallback(response) {
+                  // this callback will be called asynchronously
+                  // when the response is available
+                  return response;
+                }, function errorCallback(response) {
+                  // called asynchronously if an error occurs
+                  // or server returns response with an error status.
+                }).then(function(res){
+                  // console.log(res, 'userData from charge')
+                  $rootScope.user = res.data
+                  // console.log($rootScope.user, 'rootScope.user')
+                  $location.path('/profile')
+                  $http({
+                    method: 'GET',
+                    url: 'https://bandcamp.cc/api/send/',
+                    params: {
+                      firstName: res.data.address.name,
+                      lastName: "Amount charged: $" + res.data.amt_paid,
+                      city: "Number of students: " + res.data.studentTotal,
+                      state: "Number of tent campers: " + res.data.tentCampers,
+                      email: res.data.email,
+                      phone: "Number of cabin Campers: " + res.data.cabinCampers,
+                      comments: "Number of rv Campers: " + res.data.rvCampers
+                      }
+                  }).then(function successCallback(response) {
+                      // this callback will be called asynchronously
+                      // when the response is available
+                       // console.log(response, 'response')
+                       alert('Your payment has been processed, please fill out the camper profiles for each participant')
+                      //  $scope.user = response.data[0]
+                    }, function errorCallback(response) {
+                      // called asynchronously if an error occurs
+                      // or server returns response with an error status.
+                      alert(response)
+                    });
+                  // console.log($rootScope.user, "during callback")
+                });
+            }
             this.stripeCallback = function (code, result) {
               if (result.error) {
                 window.alert('Your card failed to process because: ' + result.error.message);
