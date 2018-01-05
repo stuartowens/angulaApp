59d113acd72465e84c93459e01d5d2190fff60cf
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-05 01:17:18 -0600

    pushes up a logout

--- public/services/signinService.js
+++ public/services/signinService.js
@@ -1,17 +1,15 @@
 angular.module('main-app')
 
 .service('signinService', function($http, $location, $rootScope) {
-  this.logout = function($rootScope) {
+  this.logout = function() {
     $http({
       method: 'GET',
       url: 'https://bandcamp.cc/api/logout/'
     }).then(function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
-         console.log(response, 'response')
          alert('You have been signed out')
-         // $location.path('/')
-         console.log($rootScope.user, "rootscope.user")
+         $location.path('/')
       }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
